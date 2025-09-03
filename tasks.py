385791d980a09e2ff8712d74c92f2b9d9333deac
ci-sourcerer <ci_sourcerer@yahoo.com>
2025-09-02 21:57:25 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,2 +1,10 @@
+import logger
+
+LOGGER = logging.getLogger(__name__)
+
 def say_hello() -> None:
     print("Hello!")
+
+def some_task(a: int, b: int) -> int:
+    LOGGER.info("Doin' some stuff")
+    return a + b
